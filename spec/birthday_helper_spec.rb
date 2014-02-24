a38da6b2e89fab63b0590ed71a31cb23c2c198f2
Kinsey Durham <kinseyann505@gmail.com>
2014-02-24 16:25:21 -0700

    Birthday Helper spec and data file

--- spec/birthday_helper_spec.rb
+++ spec/birthday_helper_spec.rb
@@ -1,12 +1,36 @@
 require 'spec_helper'
 
 describe "BirthdayHelper" do
-  it 'returns the birth date for the name that is typed in' do
+  it "returns the birth date and age of the first name typed in" do
     pending
     run_script("birthday_helper.rb")
-    type("Kinsey")
+    type("Mike")
 
-    expect(all_output).to include("Please type a name")
-    expect(all_output).to include("April 1, 1990")
+    expect(all_output).to match("May 2, 1987,
+                                27 years old")
+    expect(all_output).to_not include("April 1, 1990,
+                                      24 years old")
+  end
+
+  it "returns the birth date and age of the last name typed in" do
+    pending
+    run_script("birthday_helper.rb")
+    type("Dean")
+
+    expect(all_output).to match("November 23, 1920,
+                                94 years old")
+    expect(all_output).to_not include("May 2, 1987,
+                                      27 years old")
+  end
+
+  it "returns the birth date and age of the first and last name typed in" do
+    pending
+    run_script("birthday_helper.rb")
+    type("Kinsey Durham")
+
+    expect(all_output).to match("April 1, 1945,
+                                69 years old")
+    expect(all_output).to_not include("November 23, 1920,
+                                      94 years old")
   end
 end
