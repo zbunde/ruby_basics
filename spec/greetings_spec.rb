1584d4e6e03a78fc7fd46b507eb98e87eec93400
Mike Gehard <mike.gehard@gmail.com>
2014-03-07 09:28:29 -0700

    Add in a test to force implementation

--- spec/greetings_spec.rb
+++ spec/greetings_spec.rb
@@ -9,5 +9,12 @@ describe "Greetings" do
 
     expect(all_output).to include("What is your name?")
     expect(all_output).to include("Hello Mike!")
+
+    # Make sure we didn't hard code Mike
+    run_script("greetings.rb")
+    type("Jeff")
+
+    expect(all_output).to include("What is your name?")
+    expect(all_output).to include("Hello Jeff!")
   end
 end
